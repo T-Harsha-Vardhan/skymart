b738b4366394f147cabd462172e5f071efe73c8b
Harsha Vardhan <developerthv@gmail.com>
2026-07-20 00:57:38 +0530

    chore(api): scaffold product api module

---
+++ src/api/productApi.js
@@ -0,0 +1,9 @@
+export async function getProducts() {}
+
+export async function getProductById(id) {}
+
+export async function searchProducts(query) {}
+
+export async function getCategories() {}
+
+export async function getProductsByCategory(category) {}
