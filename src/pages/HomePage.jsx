f97305fcb9b70d78eabc3ffa5ab4af1543ecefd2
Harsha Vardhan <developerthv@gmail.com>
2026-07-19 11:27:53 +0530

    feat(ui): create Container component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,10 @@
+import Container from "../components/ui/Container";
+
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <Container>
+      <h1 className="text-4xl font-bold">Welcome to SkyMart</h1>
+      <p>Your first reusable UI component.</p>
+    </Container>
+  );
 }
