d1a7272bc59bc4a2952a05f0e1927e6c417950f2
Harsha Vardhan <developerthv@gmail.com>
2026-07-19 11:57:34 +0530

    feat(product): create ProductCard component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,14 @@
-import Button from "../components/ui/Button";
-import Card from "../components/ui/Card";
+import ProductCard from "../components/product/ProductCard";
 import Container from "../components/ui/Container";
-import Input from "../components/ui/Input";
-import Typography from "../components/ui/Typography";
 
 export default function HomePage() {
   return (
     <Container>
-      <Card>
-        <Typography variant="h2">Sign In</Typography>
-
-        <div className="mt-6 space-y-4">
-          <Input placeholder="Email" />
-
-          <Input type="password" placeholder="Password" />
-
-          <Button>Login</Button>
-        </div>
-      </Card>
+      <ProductCard
+        title="iPhone 17 Pro"
+        price={89999}
+        image="https://picsum.photos/300"
+      />
     </Container>
   );
 }
